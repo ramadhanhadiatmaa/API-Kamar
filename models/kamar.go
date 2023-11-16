e11785567163ca62b027a5c7e67c7005921ec86c
kodlitecom <nandaaprilia605@gmail.com>
2023-11-17 05:02:18 +0700

    refactor(model): Change Structure Model

--- models/kamar.go
+++ models/kamar.go
@@ -4,15 +4,15 @@ import "time"
 
 type Kamar struct {
 	Id          string    `gorm:"type:varchar(20); primaryKey" json:"id"`
-	MarkusNicu  string    `gorm:"type:varchar(20)" json:"markus_nicu"`
-	MarkusVvip  string    `gorm:"type:varchar(20)" json:"markus_vvip"`
-	MarkusVip   string    `gorm:"type:varchar(20)" json:"markus_vip"`
+	Markusnicu  string    `gorm:"type:varchar(20)" json:"markusnicu"`
+	Markusvvip  string    `gorm:"type:varchar(20)" json:"markusvvip"`
+	Markusvip   string    `gorm:"type:varchar(20)" json:"markusvip"`
 	Lukas       string    `gorm:"type:varchar(20)" json:"lukas"`
 	Maria       string    `gorm:"type:varchar(20)" json:"maria"`
 	Fransiskus  string    `gorm:"type:varchar(20)" json:"fransiskus"`
 	Matius      string    `gorm:"type:varchar(20)" json:"matius"`
 	Teresia     string    `gorm:"type:varchar(20)" json:"teresia"`
-	TeresiaTiga string    `gorm:"type:varchar(20)" json:"teresia_tiga"`
+	Teresiatiga string    `gorm:"type:varchar(20)" json:"teresiatiga"`
 	Yosef       string    `gorm:"type:varchar(20)" json:"yosef"`
 	Klara       string    `gorm:"type:varchar(20)" json:"klara"`
 	Egidio      string    `gorm:"type:varchar(20)" json:"egidio"`
